a46f68201caa1840f8ee75f0865c615c64a0e97e
TESLA2402 <lakshay6447@gmail.com>
2022-06-24 15:53:20 +0530

    completed

---
+++ chat_box/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:chat_box/chat.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: ChatScreen(),
+  ));
+}
